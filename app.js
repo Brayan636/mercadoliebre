a96e31fa20eb0fb96fa5e2a28998c43586b67ec0
Brayan63 <brayan-garcia12345@hotmail.com>
2021-07-07 07:58:05 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+let express = require("express");
+const app = express();
+app.use("/static", express.static(__dirname + "/public"));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/home.html');
+})
+app.get('/register', (req, res) => {
+    res.sendFile(__dirname + '/views/register.html');
+})
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/views/login.html');
+})
+app.listen(process.env.PORT || 3000, function() {
+    console.log("jalando al 100") 
+})
